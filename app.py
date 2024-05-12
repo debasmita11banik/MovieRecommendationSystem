e9fae1df05f818d71bfe02f11a28372964f688f5
tonydeb42 <tonydeb42@gmail.com>
2024-05-13 02:51:04 +0530

    recommendations table styling fixed

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template,request,redirect
+from flask import Flask, render_template, request, redirect
 import recommendation_system as rs
 
 app = Flask(__name__)
@@ -9,21 +9,24 @@ def index():  # put application's code here
     return render_template("index.html")
 
 
-@app.route('/login',methods=['GET','POST'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    username=request.form.get('username')
-    password=request.form.get('password')
-    if username=="user1" and password=="admin@12345":
+    username = request.form.get('username')
+    password = request.form.get('password')
+    if username == "user1" and password == "admin@12345":
         return render_template("user_input.html")
     else:
         return render_template("index.html")
 
-@app.route('/userinput',methods=['GET','POST'])
+
+@app.route('/userinput', methods=['GET', 'POST'])
 def userInput():
-    genere=request.form.get('genere')
-    language=request.form.get('language')
-    data=rs.run(genere,language)
-    return data.to_html()
+    genere = request.form.get('genere')
+    language = request.form.get('language')
+    data = rs.run(genere, language)
+    # return data.to_html()
+    table_html = data.to_html(index=False)
+    return render_template('recommendations.html', table_data=table_html)
 
 
 if __name__ == '__main__':
